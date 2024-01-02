6124204d0d2510f197f54276620b828d5a8755ae
g_de0k <akg13078@naver.com>
2024-01-02 14:53:17 +0900

    2주차 django 개발 시작

---
+++ mysite/views.py
@@ -0,0 +1,10 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+from .models import MainContent
+
+def index(request):
+    # return HttpResponse("Hello world")
+
+    content_list = MainContent.objects.order_by('-pub_date')
+    context = {'content_list': content_list}
+    return render(request, 'mysite/content_list.html', context)
